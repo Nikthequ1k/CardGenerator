f19d745984229901098ba9b66efc7db15beab419
Nick <166399810+Nikthequ1k@users.noreply.github.com>
2024-05-30 00:43:40 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
 import "bootstrap";
-import "./style.css";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const suits = ["♥", "♦", "♣", "♠"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateCard() {
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  document.getElementById("card-suit-top").innerText = randomSuit;
+  document.getElementById("card-suit-bottom").innerText = randomSuit;
+  document.getElementById("card-value").innerText = randomValue;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    document.getElementById("card-suit-top").style.color = "red";
+    document.getElementById("card-suit-bottom").style.color = "red";
+    document.getElementById("card-value").style.color = "red";
+  } else {
+    document.getElementById("card-suit-top").style.color = "black";
+    document.getElementById("card-suit-bottom").style.color = "black";
+    document.getElementById("card-value").style.color = "black";
+  }
+}
+const generateCardbutton = document.querySelector(".generateCardbutton");
+generateCardbutton.addEventListener("click", generateCard);
